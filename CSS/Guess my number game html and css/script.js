d06450fb74bd3bba2c69d1f19684bba0ce819219
Unknownmaster0 <sagar192002singh@gmail.com>
2024-04-25 18:05:43 +0530

    CSS learning

---
+++ CSS/Guess my number game html and css/script.js
@@ -0,0 +1,5 @@
+// how to select the element of the HTML in JS.
+console.log(document.querySelector(".message").textContent);
+console.log(document.querySelector("h1").textContent);
+console.log(document.querySelector("footer").textContent);
+console.log(document.querySelector(".label-score").textContent);
